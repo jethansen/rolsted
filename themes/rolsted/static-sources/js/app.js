ad3be9b01acd928dd594df1475b284a09e8cae36
Jesper Hansen <jh@audienceproject.com>
2018-04-18 21:38:34 +0200

    no message

--- themes/rolsted/static-sources/js/app.js
+++ themes/rolsted/static-sources/js/app.js
@@ -4,5 +4,6 @@
 // @codekit-append  "barba.js"
 // @codekit-append  "scroll-restoration-polyfill.browser.js"
 // @codekit-append  "jquery.easing.1.3.js"
+// @codekit-append  "lazysizes.min.js"
 // @codekit-append  "functions.js"
 // @codekit-append  "main.js"
